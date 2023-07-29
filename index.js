c9e3a73a2aa4acb8af5c707bf5bd853627778823
Andrew <andbostick@gmail.com>
2023-07-29 18:29:48 -0400

    added license functions and structure of readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs');
-const generate = require('./utils/generateMarkdown')
+const generate = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -13,6 +13,11 @@ const questions = [
         type:'input',
         name:'description',
         message: 'what is the description of your project?'
+    },
+    {
+        type:'input',
+        name:'install',
+        message: 'What is the Installtion information for your project?'
     },
     {
         type:'input',
@@ -33,23 +38,31 @@ const questions = [
         type:'list',
         name:'license',
         message: 'What license are you using?',
-        choices:['MIT', 'Mozilla','ISC','IBM']
+        choices:['MIT', 'ISC','IBM', 'None']
     }
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.md`,data, err => {
+        if(err){
+            console.error(err)
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers.title)
         console.log(answers.description)
+        console.log(answers.install)
         console.log(answers.usage)
         console.log(answers.guidelines)
         console.log(answers.testing)
         console.log(answers.license)
+        writeToFile('README', generate(answers))
     })
 }
 
